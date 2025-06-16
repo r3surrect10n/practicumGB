5932bfb2f438d02370f2af73650f550100f72202
alex23job <106354657+alex23job@users.noreply.github.com>
2025-06-17 02:04:10 +0300

    Adding Player rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField, Range(0, 10)] private float _movementSpeed;
-
+    [SerializeField, Range(0, 20)] private float rotationSmoothness = 10f;    // Коэффициент плавности поворота
     private CharacterController _playerController;
 
     private Vector3 _moveInput; 
@@ -19,8 +19,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update()
-    {        
-        Vector3 movement = _moveInput.x * transform.right + _moveInput.y * transform.forward + Gravity * transform.up;  
+    {
+        // Нормализованное направление движения
+        Vector3 moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+        moveDirection.Normalize();
+
+        // Плавный поворот в сторону движения
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            _playerController.transform.rotation = Quaternion.Slerp(_playerController.transform.rotation, targetRotation, rotationSmoothness * Time.deltaTime);
+        }
+        //Vector3 movement = _moveInput.x * transform.right + _moveInput.y * transform.forward + Gravity * transform.up;
+        Vector3 movement = moveDirection + Gravity * transform.up;
 
         _playerController.Move(movement *  _movementSpeed * Time.deltaTime);
     }
