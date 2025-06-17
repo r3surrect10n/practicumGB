c0c8c62c06515b30089e78769d6c0b910a300c34
r3suurect10n <sosnin.a.a@yandex.ru>
2025-06-17 22:48:11 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0, 10)] private float _movementSpeed;
-    [SerializeField, Range(0, 20)] private float rotationSmoothness = 10f;    // Коэффициент плавности поворота
+    [Header("Movement and rotation speeds")]
+    [SerializeField, Range(0, 10)] private float _movementSpeed;    //Скорость передвижения персонажа
+    [SerializeField, Range(0, 20)] private float _rotationSmoothness = 10f;    // Коэффициент плавности поворота
+
+    private const float Gravity = -9.81f;  
+    
     private CharacterController _playerController;
 
     private Vector3 _moveInput; 
-
-    private const float Gravity = -9.81f;        
 
     private void Awake()
     {
@@ -28,15 +30,14 @@ public class PlayerMovement : MonoBehaviour
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            _playerController.transform.rotation = Quaternion.Slerp(_playerController.transform.rotation, targetRotation, rotationSmoothness * Time.deltaTime);
+            _playerController.transform.rotation = Quaternion.Slerp(_playerController.transform.rotation, targetRotation, _rotationSmoothness * Time.deltaTime);
         }
-        //Vector3 movement = _moveInput.x * transform.right + _moveInput.y * transform.forward + Gravity * transform.up;
+        
         Vector3 movement = moveDirection + Gravity * transform.up;
-
         _playerController.Move(movement *  _movementSpeed * Time.deltaTime);
     }
-
-    public void OnMove(InputAction.CallbackContext callbackContext)
+        
+    public void OnMove(InputAction.CallbackContext callbackContext) //Прием инпута
     {
         _moveInput = callbackContext.ReadValue<Vector2>();
     }
