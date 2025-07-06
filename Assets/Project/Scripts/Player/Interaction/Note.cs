c7b93dc6235da38ea43dd3bc4c8837b559eef4d2
r3suurect10n <sosnin.a.a@yandex.ru>
2025-07-07 02:09:34 +0300

    Add notes sounds

--- Assets/Project/Scripts/Player/Interaction/Note.cs
+++ Assets/Project/Scripts/Player/Interaction/Note.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class Note : MonoBehaviour, IInteractable
 {
+    [SerializeField] private AudioSource _canvasAudioSource;
+
+    [SerializeField] private AudioClip _noteTakeClip;
+    [SerializeField] private AudioClip _notePickClip;
+    
     [SerializeField] private GameObject _note;
 
     public void Interact()
     {
         _note.SetActive(true);        
+        _canvasAudioSource.PlayOneShot(_noteTakeClip);
     }
 
     public void EndInteract()
     {
         _note.SetActive(false);
+        _canvasAudioSource.PlayOneShot(_notePickClip);
         gameObject.SetActive(false);
     }
 
